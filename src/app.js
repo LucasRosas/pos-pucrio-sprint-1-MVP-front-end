910a2a746f8ea33c6bead1ef6ec58a6d20623004
Lucas Rosas <lucasrosas@outlook.com>
2025-03-08 18:56:27 -0300

    reestilização e reserva de quadra

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-document.title = "O código funciona";
-
 document.getElementById("avatar").style.backgroundImage =
   "url('https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
 
+const zeroPad = (num, places) => String(num).padStart(places, "0");
+
 class Auth {
   constructor() {
+    this.schedules = [];
     this.token = localStorage.getItem("token");
     this.loginModal = document.getElementById("loginModal");
     if (!this.token) {
@@ -116,13 +117,14 @@ class Calendar {
             "data-day",
             new Date(this.year, this.month, day).toISOString()
           );
+          td.addEventListener("click", (event) => {
+            this.openScheduleModal(event);
+          });
           if (
             day === new Date().getDate() &&
             this.month === new Date().getMonth() &&
             this.year === new Date().getFullYear()
           ) {
-            console.log("today");
-            console.log(day);
             td.innerHTML = `<div class='day today'>${day}</div>`;
           } else {
             td.innerHTML = `<div class='day'>${day}</div>`;
@@ -139,8 +141,91 @@ class Calendar {
       new Date().setMonth(this.month)
     ).toLocaleDateString("pt-BR", { month: "long" });
     let thisYear = new Date(new Date().setFullYear(this.year)).getFullYear();
-    this.calendarElement.innerHTML = `<h2>${thisMonth}/${thisYear}</h2>`;
+    let currentMonthElement = document.getElementById("currentMonth");
+    currentMonthElement.innerHTML = `<h2>${thisMonth} de ${thisYear}</h2>`;
+    this.calendarElement.innerHTML = "";
     this.calendarElement.appendChild(table);
+    this.plotSchedules();
+  };
+
+  getSchedules = () => {
+    this.schedules = [
+      {
+        date: new Date().toISOString(),
+        mine: true,
+        title: "Reunião com a equipe",
+        description: "Reunião para discutir o planejamento do próximo sprint",
+      },
+      {
+        date: new Date(2025, 2, 10, 15, 0).toISOString(),
+        mine: false,
+        title: "Reunião com a equipe",
+        description: "Reunião para discutir o planejamento do próximo sprint",
+      },
+    ];
+
+    this.plotSchedules();
+  };
+
+  clearSchedules = () => {
+    let schedules = document.querySelectorAll(".schedule");
+    schedules.forEach((schedule) => {
+      schedule.innerHTML = "";
+    });
+  };
+
+  plotSchedules = () => {
+    this.clearSchedules();
+    this.schedules.forEach((schedule) => {
+      let year = new Date(schedule.date).getFullYear();
+      let month = new Date(schedule.date).getMonth();
+      let day = new Date(schedule.date).getDate();
+      let hour = new Date(schedule.date).getHours();
+      let min = zeroPad(new Date(schedule.date).getMinutes(), 2);
+      let dayElement = document.querySelector(
+        `[data-day='${new Date(year, month, day).toISOString()}']`
+      );
+      console.log("dia encontrado", dayElement);
+      if (dayElement) {
+        let childElement = dayElement.querySelector(".day");
+        let scheduleElement = document.createElement("div");
+        scheduleElement.classList.add("schedule");
+        if (!schedule.mine) {
+          scheduleElement.classList.add("schedule-other");
+          scheduleElement.innerHTML = `<div class='icon prohibit'></div>${hour}h${min} - ${
+            hour + 1
+          }h${min}`;
+        } else {
+          scheduleElement.innerHTML = `<div class='icon ball'></div>${hour}h${min} - ${
+            hour + 1
+          }h${min}`;
+        }
+        childElement.appendChild(scheduleElement);
+      }
+    });
+  };
+
+  openScheduleModal = (event) => {
+    document.getElementById("scheduleModal").showModal();
+    if (event.target.classList.contains("day")) {
+      let date = new Date(event.target.parentElement.getAttribute("data-day"));
+      let inputDate = document.getElementById("inputDate");
+      inputDate.value = new Date(date).toISOString().split("T")[0];
+      let inputTime = document.getElementById("inputTime");
+      inputTime.focus();
+    }
+  };
+
+  onSchedule = (event) => {
+    let date = document.getElementById("inputDate").value;
+    let time = document.getElementById("inputTime").value;
+    let players = document.getElementById("inputPlayers").value;
+    this.schedules.push({
+      date: new Date(`${date}T${time}`).toISOString(),
+      mine: true,
+    });
+    console.log(date, time, players, event);
+    this.plotSchedules();
   };
 }
 
